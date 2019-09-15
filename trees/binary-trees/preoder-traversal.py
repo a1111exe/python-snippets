d38affc89dbe88ca13b16e904a104b4f20da2148
a1111exe <a1111exe@gmail.com>
2019-09-15 19:40:49 +0300

    Update preoder-traversal.py
    Added tests for None to not descend into 'None' nodes

--- trees/binary-trees/preoder-traversal.py
+++ trees/binary-trees/preoder-traversal.py
@@ -13,7 +13,7 @@ class Solution:
         
         while stack:
             node = stack[0][0]
-        
+
             if node is None:
                 # print('None, ascending')
                 del(stack[0])
@@ -23,21 +23,32 @@ class Solution:
             rvisited = stack[0][2]
 
             if node.val is not None and not (lvisited or rvisited):
-                # print('Appending {}'.format(node.val))
+                # 1st time visited: traversing value
                 traversal.append(node.val)
 
             if not lvisited:
-                # print('Descending left')
+                # Left node now visited, mark so
                 stack[0][1] = True
 
-                stack.insert(0, [ node.left, False, False ])
+                if node.left is not None:
+                    # Descending into the left node
+                    stack.insert(0, [ node.left, False, False ])
+                else:
+                    # Going to descent into the right node on the next iteration
+                    continue
             elif not rvisited:
-                # print('Descending right')
+                # Right node now visited, mark so
                 stack[0][2] = True
-
-                stack.insert(0, [ node.right, False, False ])
+                
+                if node.right is not None:
+                    # Descending into the right node
+                    stack.insert(0, [ node.right, False, False ])
+                else:
+                    # Ascending
+                    del(stack[0])
+                    continue
             else:
-                # print('Both visited, ascending')
+                # Both visited, ascending
                 del(stack[0])
 
         return traversal
